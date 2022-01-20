2172431b2703dc3ea453edad67d6f3600145efa1
cristianvogel <cristian.vogel55@gmail.com>
2022-01-20 13:33:55 +0100

    netlify feedback form tag fix

--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -27,7 +27,7 @@ class FeedbackForm extends React.Component {
         fetch("/", {
             method: "POST",
             headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: encode({ "form-name": "contact", ...this.state })
+            body: encode({ "form-name": "feedbackForm", ...this.state })
         })
             .then(() => alert("Success!!"))
             .catch(error => alert(error));
