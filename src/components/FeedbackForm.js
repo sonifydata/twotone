8d1c1e9cbc4b204ba5930b44b708d4239c0661b4
cristianvogel <cristian.vogel55@gmail.com>
2022-01-20 11:11:51 +0100

    netlify feedback form

--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -57,7 +57,7 @@ class FeedbackForm extends React.Component {
             </div>
 
             <div className="container is-fluid pt-6 hero is-dark">
-                <form name="feedbackForm" netlify="true" netlify-honeypot="bot-field"  method="POST" onSubmit={this.handleSubmit} >
+                <form name="feedbackForm" data-netlify="true" netlify-honeypot="bot-field"  method="POST" onSubmit={this.handleSubmit} >
                     <input aria-hidden="true" type="hidden" name="form-name" value="feedbackForm" />
                     <div className="field">
                         <label className="has-text-light">Name</label>
